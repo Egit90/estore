963ae00cbf4e4a46b779a2f81a6e97155f3a3851
egit90 <elie90@gmail.com>
2024-01-08 20:52:06 -0500

    init

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(StoreContext context)
+{
+    private readonly StoreContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var Products = await _context.Products.ToListAsync();
+        return Products;
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProducts(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+
+}
---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,8 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
